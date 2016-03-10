7a1187af4dd56dc103b040e9397bfb060e2b8ab2
Florian Curinga <curinga@kth.se>
2016-03-10 17:38:05 +0100

    filter v1.1: added adaptative thresholding, 100% success with the old images

--- testfilter.py
+++ testfilter.py
@@ -8,5 +8,3 @@ onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 for e in onlyfiles:
     filt.filter(e, mypath+"/", "/home/florian/Images/filtered/")
-
-#filt.filter("30786-30785.jpg", mypath+"/", "/home/florian/Images/filtered/")
